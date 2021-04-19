6a3f0b272aa8171221d9b5031959eef690575374
GMFelixfex <felix.poenitzsch@hs-furtwangen.de>
2021-04-19 22:26:37 +0200

    Updated to v1.0.3

--- Multicash/MyMod.cs
+++ Multicash/MyMod.cs
@@ -1,24 +1,35 @@
 ﻿using MelonLoader;
 using Harmony;
-using NKHook6;
 using Assets.Scripts.Simulation;
-using NKHook6.Api.Events;
 using Assets.Scripts.Unity.UI_New.Popups;
+using Assets.Scripts.Unity.UI_New.Settings;
 using TMPro;
+using System;
+using Assets.Scripts.Unity.UI_New.InGame;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Drawing;
+using System.IO;
+using Assets.Scripts.Unity;
+using Assets.Scripts.Unity.UI_New.InGame.TowerSelectionMenu;
+
+
+
+
+
 
 namespace Multicash
 {
     public class MyMod : MelonMod
     {
         static float multi = 2;
-
-        static string holdKey = "";
+        static bool Hotkeyd;
+        public static bool definitelyTryPlaceNext;
 
         public override void OnApplicationStart()
         {
             base.OnApplicationStart();
-            EventRegistry.instance.listen(typeof(MyMod));
-            Logger.Log("Multi-Cash loaded");
+            MelonLogger.Log("multicash loaded");
         }
         [HarmonyPatch(typeof(Simulation), "AddCash")]
         public class Mcash
@@ -31,67 +42,47 @@ namespace Multicash
 
             }
         }
+
         public override void OnUpdate()
         {
             base.OnUpdate();
 
-            if (testForActivity)
+            if (Hotkeyd)
             {
                 if (PopupScreen.instance.GetFirstActivePopup() != null)
                 {
                     PopupScreen.instance.GetFirstActivePopup().GetComponentInChildren<TMP_InputField>().characterValidation = TMP_InputField.CharacterValidation.None;
-                    testForActivity = false;
+                    Hotkeyd = false;
+                }
+                
+            }
+            
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                if (Input.GetKeyDown(KeyCode.F10))
+                {
+                    Il2CppSystem.Action<string> mod = (Il2CppSystem.Action<string>)delegate (string s)
+                    {
+                        multi = float.Parse(s);
+
+                    };
+                    PopupScreen.instance.ShowSetNamePopup("Cash", "Multiply Cash by", mod, "2");
+                    
+                    Hotkeyd = true;
                 }
 
             }
-        }
-        static bool testForActivity;
 
-        [EventAttribute("KeyHeldEvent")]
-        public static void onHoldEvent(KeyEvent e)
-        {
 
-            string key = e.key + "";
-            if (key == "LeftShift")
-            {
-                holdKey = "LeftShift";
-            }
 
-        }
+
+
+
         
-        [EventAttribute("KeyReleaseEvent")]
-        public static void onReleaseEvent(KeyEvent e)
-        {
+        
+    }
 
-            string key = e.key + "";
-            if (key == "LeftShift")
-            {
-                holdKey = "";
-            }
-
-        }
-
-        [EventAttribute("KeyPressEvent")]
-        public static void onKeyEvent(KeyEvent e)
-        {
-
-            string key = e.key + "";
-
-            if (key == "F10" && holdKey == "LeftShift")
-            {
-                Il2CppSystem.Action<string> mod = (Il2CppSystem.Action<string>)delegate (string s)
-                {
-                    multi = float.Parse(s);
-
-                };
-                PopupScreen.instance.ShowSetNamePopup("Multicash", "Multiply Cash by", mod, "2");
-                testForActivity = true;
-            }
-
-
-
-
-        }
 
     }
 }
